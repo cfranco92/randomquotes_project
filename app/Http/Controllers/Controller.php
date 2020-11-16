a9aed286fc6736d9f9e1f0ac5f2ae35f80aa02a5
Cristian Franco <cfrancobedoya@gmail.com>
2020-11-16 10:43:21 -0500

    Push

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -24,10 +24,10 @@ class Controller extends BaseController
 
     public function show()
     {
+        // In this case, we're creating random url of our images saved in aws s3
         $randomNumber = (rand(1,15));
         $url = "https://cfranco-randomquotes.s3.amazonaws.com/image{$randomNumber}.jpeg";
         $randomQuoteURL = $url;
         return response()->json(['quoteImageURL' => $randomQuoteURL, 'server_ip' => gethostbyname(gethostname())]);
     }
-
 }
