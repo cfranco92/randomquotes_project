6d8eee56f3d649404f37e5e8fea64b44c5e141f2
Cristian Franco <cfrancobedoya@gmail.com>
2020-11-16 11:03:29 -0500

    Adding index route with new function.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,10 +16,15 @@ class Controller extends BaseController
 
     public function index()
     {
-        $totalQuotes = (count(Controller::$quotes));
-        $randomNumber = (rand(0,($totalQuotes-1)));
-        $randomQuote = Controller::$quotes[$randomNumber];
-        return response()->json(['quote' => $randomQuote, 'server_ip' => gethostbyname(gethostname())]);
+        // $totalQuotes = (count(Controller::$quotes));
+        // $randomNumber = (rand(0,($totalQuotes-1)));
+        // $randomQuote = Controller::$quotes[$randomNumber];
+        // return response()->json(['quote' => $randomQuote, 'server_ip' => gethostbyname(gethostname())]);
+        // In this case, we're creating random url of our images saved in aws s3
+        $randomNumber = (rand(1,15));
+        $url = "https://cfranco-randomquotes.s3.amazonaws.com/image{$randomNumber}.jpeg";
+        $randomQuoteURL = $url;
+        return response()->json(['quoteImageURL' => $randomQuoteURL, 'server_ip' => gethostbyname(gethostname())]);
     }
 
     public function show()
